bdbd67114d29744f4b7b04c3897c6e20215ac024
desai-malvika <malvika@webkorps.com>
2021-06-15 16:16:44 +0530

    Array examples

---
+++ Array/Example1.rb
@@ -0,0 +1,7 @@
+color = ["Red", "Green", "Blue", "White"]
+puts "Original array:\n"
+puts color
+puts "\nCheck if 'Green' in color array!\n"
+puts color.include? 'Green'
+puts "\nCheck if 'Pink' in color array!\n"
+puts color.include? 'Pink'
---
+++ Array/Example2.rb
@@ -0,0 +1,7 @@
+nums = [12, 34, 23, 56]
+puts "Original array:\n"
+puts nums
+puts "\n 2 random elements from the array.\n"
+puts nums.sample(2)
+puts "\n 3 random elements from the array.\n"
+puts nums.sample(3)
